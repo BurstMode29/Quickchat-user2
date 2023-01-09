a08d1661cf785975f7c67704d64d40935def0069
Rafeeq Weideman <r.w.weideman.rafeeq@gmail.com>
2023-01-09 14:49:38 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import { addDoc, collection, deleteDoc, getDocs, doc } from 'firebase/firestore';
+import { db } from './firebase';
+import { useEffect, useState } from 'react';
+import { async } from '@firebase/util';
 
 function App() {
+  const [messages, updateMessages] = useState([]);
+  const [messages2, updateMessages2] = useState([]);
+  const [text, setText] = useState('');
+
+  // Read Data
+  const readData = async () => {
+    await getDocs(collection(db, "messages"))
+      .then((querySnapshot) => {
+        const newData = querySnapshot.docs
+          .map((doc) => ({...doc.data(), id:doc.id}));
+        updateMessages(newData);
+      })
+  }
+
+  useEffect(() => {
+    readData();
+  }, [])
+
+  const sendData = async () => {
+    await getDocs(collection(db, "user-2"))
+      .then((querySnapshot) => {
+        const sentData = querySnapshot.docs
+         .map((doc) => ({...doc.data(), id:doc.id}));
+        updateMessages2(sentData);
+      })
+  }
+
+  useEffect(() => {
+    sendData();
+  })
+
+  // Write Data
+  const addText = async (e) => {
+    e.preventDefault(e)
+
+    if(text === '' ) {
+      alert('please complete fields')
+      return
+    }
+
+    await addDoc(collection(db, 'user-2'),  {
+      text: text,
+    })
+    setText('');
+    window.location.reload(false);
+  }
+
+  // Delete Data
+  const deleteMessage = (id) => {
+      console.log(id);
+      console.log();
+    deleteDoc(doc(db, "user-2", id))
+    .then(() => {
+      // alert("Message Deleted");
+      window.location.reload(false);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='block'>
+        <div className='messages'>
+          {messages?.map((data, i) =>(
+            <div className='user-1' key={i}>
+              {/* <a className='deleteMessage' key={data.id} onClick={() => deleteMessage(data.id)} href="#">X</a> */}
+              {data.text}
+            </div>
+          ))}
+
+          {messages2?.map((data, i) =>(
+            <div className='user-2' key={i}>
+              <a className='deleteMessage' key={data.id} onClick={() => deleteMessage(data.id)} href="#">X</a>
+              {data.text}
+            </div>
+          ))}
+          
+        </div>
+        <div className='typeArea'>
+          <input value={text} onChange={(e) => setText(e.target.value)} type='text' placeholder='type'></input>
+          <button onClick={addText} type='submit'>SEND</button>
+        </div>
+      </div>
     </div>
   );
 }
